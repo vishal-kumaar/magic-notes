7ab659072ac4f990342ff6093e063e83002aaa56
Vishal Kumar <vy10607@gmail.com>
2023-01-22 22:43:09 +0530

    Fixed Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Footer() {
+export default function Footer(props) {
   return (
     <div className='fixed w-full bottom-0'>
-        <div className='font-semibold text-center bg-gray-700 text-white'>
+        <div className={`py-1 font-semibold text-center text-white ${props.mode==="light" ? "bg-gray-700" : "bg-gray-900"}`}>
             Copyright &copy; Magic Todo 2023
         </div>
     </div>
