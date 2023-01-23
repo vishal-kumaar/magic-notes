674fb88b5134078e34072fbb2ae268fad544a34a
Vishal Kumar <vy10607@gmail.com>
2023-01-23 14:36:42 +0530

    Added dark mode in Signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,36 +1,88 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import left_arrow from "../assets/images/left_arrow.svg";
+import React from "react";
+import { Link } from "react-router-dom";
+import leftArrow from "../assets/images/left_arrow.svg";
 
 export default function Signup(props) {
   return (
     <div>
-        <div className='mt-4 ml-2'>
-            <Link to="/profile">
-                <img src={left_arrow} alt="left-arrow" className={`w-7 h-7 ${props.mode==="light" ? "invert-0" : "invert"}`}/>
-            </Link>
+      <div className="mt-4 ml-2">
+        <Link to="/login">
+          <img
+            src={leftArrow}
+            alt="left-arrow"
+            className={`w-7 h-7 ${
+              props.mode === "light" ? "invert-0" : "invert"
+            }`}
+          />
+        </Link>
+      </div>
+      <form className="mx-4 sm:mx-10 md:mx-20 lg:mx-36 xl:mx-72 2xl:mx-96 my-10">
+        <div
+          className={`font-extrabold text-2xl mb-7 text-center ${
+            props.mode === "light" ? "text-black" : "text-white"
+          }`}
+        >
+          Sign Up
         </div>
-        <form className='mx-4 sm:mx-10 md:mx-20 lg:mx-36 xl:mx-72 2xl:mx-96 mt-4 mb-10'>
-        <div className='font-extrabold text-2xl mb-7 text-center underline'>Sign Up</div>
-        <div className='my-6'>
-            <input type="text" placeholder='Your Full Name' className={`w-full border-2 py-3 px-6 font-lg outline-none rounded-3xl border-gray-200 text-gray-500 placeholder:text-gray-500`}/>
+        <div className="my-6">
+          <input
+            type="text"
+            placeholder="Your Full Name"
+            className={`w-full border-2 py-3 px-6 font-lg outline-none rounded-3xl shadow-md ${
+              props.mode === "light"
+                ? "border-gray-200 bg-white text-gray-500 placeholder:text-gray-500"
+                : "border-gray-400 bg-gray-600 text-gray-100 placeholder:text-gray-100"
+            }`}
+          />
         </div>
-        <div className='my-6'>
-            <input type="email" placeholder='Your Email Address' className={`w-full border-2 py-3 px-6 font-lg outline-none rounded-3xl border-gray-200 text-gray-500 placeholder:text-gray-500`}/>
+        <div className="my-6">
+          <input
+            type="email"
+            placeholder="Your Email Address"
+            className={`w-full border-2 py-3 px-6 font-lg outline-none rounded-3xl shadow-md ${
+              props.mode === "light"
+                ? "border-gray-200 bg-white text-gray-500 placeholder:text-gray-500"
+                : "border-gray-400 bg-gray-600 text-gray-100 placeholder:text-gray-100"
+            }`}
+          />
         </div>
-        <div className='my-6'>
-            <input type="password" placeholder='Password' className={`w-full border-2 py-3 px-6 font-lg outline-none rounded-3xl border-gray-200 text-gray-500 placeholder:text-gray-500`}/>
+        <div className="my-6">
+          <input
+            type="password"
+            placeholder="Password"
+            className={`w-full border-2 py-3 px-6 font-lg outline-none rounded-3xl shadow-md ${
+              props.mode === "light"
+                ? "border-gray-200 bg-white text-gray-500 placeholder:text-gray-500"
+                : "border-gray-400 bg-gray-600 text-gray-100 placeholder:text-gray-100"
+            }`}
+          />
         </div>
-        <div className='my-6'>
-            <input type="password" placeholder='Confirm Password' className={`w-full border-2 py-3 px-6 font-lg outline-none rounded-3xl border-gray-200 text-gray-500 placeholder:text-gray-500`}/>
+        <div className="my-6">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className={`w-full border-2 py-3 px-6 font-lg outline-none rounded-3xl shadow-md ${
+              props.mode === "light"
+                ? "border-gray-200 bg-white text-gray-500 placeholder:text-gray-500"
+                : "border-gray-400 bg-gray-600 text-gray-100 placeholder:text-gray-100"
+            }`}
+          />
         </div>
-        <div className='my-6'>
-            <button type="submit" className='border-2 w-full py-3 rounded-3xl font-semibold text-lg bg-blue-500 text-white'>Sign Up</button>
+        <div className="my-6">
+          <button
+            type="submit"
+            className={`w-full py-3 rounded-3xl font-semibold text-lg bg-blue-500 text-white shadow-lg`}
+          >
+            Sign up
+          </button>
         </div>
-        </form>
-        <div className='text-center mt-28 font-medium text-gray-400'>
-            Already have an account?<Link><span className='text-blue-500'> Log in</span></Link>
-        </div>
+      </form>
+      <div className={`text-center mt-24 font-medium text-gray-400`}>
+        Already have an account?
+        <Link to="/login">
+          <span className="text-blue-500"> Log in</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
