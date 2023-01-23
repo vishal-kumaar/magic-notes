085fa4038e542631817cf162aafd191605544732
Vishal Kumar <vy10607@gmail.com>
2023-01-23 14:36:17 +0530

    Fixed UserProfile

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -6,6 +6,7 @@ import logoutIcon from "../assets/images/logout_icon.svg";
 import leftArrow from "../assets/images/left_arrow.svg";
 import rightArrow from "../assets/images/right_arrow.svg";
 import colorMode from "../assets/images/color_mode.png"
+import loginIcon from "../assets/images/login_icon.svg"
 import { Link } from "react-router-dom";
 
 export default function UserProfile(props) {
@@ -16,15 +17,15 @@ export default function UserProfile(props) {
             <img src={colorMode} alt="color-mode" className={`w-7 h-7 cursor-pointer ${props.mode==="light" ? "invert-0" : "invert"}`} onClick={props.toggleMode}/>
         </div>
       <div className="flex flex-col items-center justify-center mx-8 mt-2">
-        <div className="">
+        <div className="/updateName">
           <img src={profileImg} alt="profile-pic" className="w-32 h-32" />
         </div>
         <div className={`text-2xl font-bold mt-4 ${props.mode==="light" ? "text-black" : "text-white"}`}>Vishal Kumar</div>
         <div className={`text-lg ${props.mode==="light" ? "text-gray-500" : "text-gray-400"}`}>vishal.kumar@gmail.com</div>
       </div>
-      <div className="mt-10">
-        <Link>
-          <div className={`flex items-center justify-between shadow-md rounded-2xl px-2 py-1 mx-8 my-4 ${props.mode==="light" ? "text-gray-700 bg-gray-100" : "text-gray-300 bg-gray-700"}`}>
+      <div className="mt-10 mx-8 sm:mx-16 md:mx-24 lg:mx-36 xl:mx-56 2xl:mx-96">
+        <Link to="/updateName">
+          <div className={`flex items-center justify-between shadow-md rounded-2xl px-2 py-1 my-4 ${props.mode==="light" ? "text-gray-700 bg-gray-100" : "text-gray-300 bg-gray-700"}`}>
             <div className="flex items-center">
               <img src={userIcon} alt="user-icon" className={`w-8 h-8 ${props.mode==="light" ? "invert-0" : "invert"}`} />
               <div className="ml-2">Update Name</div>
@@ -32,8 +33,8 @@ export default function UserProfile(props) {
             <img src={rightArrow} alt="right-arrow" className={`w-6 h-6 ${props.mode==="light" ? "invert-0" : "invert"}`} />
           </div>
         </Link>
-        <Link>
-          <div className={`flex items-center justify-between shadow-md rounded-2xl px-2 py-1 mx-8 my-4 ${props.mode==="light" ? "text-gray-700 bg-gray-100" : "text-gray-300 bg-gray-700"}`}>
+        <Link to="/updatePassword">
+          <div className={`flex items-center justify-between shadow-md rounded-2xl px-2 py-1 my-4 ${props.mode==="light" ? "text-gray-700 bg-gray-100" : "text-gray-300 bg-gray-700"}`}>
             <div className="flex items-center">
               <img src={passwordIcon} alt="user-icon" className={`w-8 h-8 ${props.mode==="light" ? "invert-0" : "invert"}`} />
               <div className="ml-2">Update Password</div>
@@ -41,11 +42,20 @@ export default function UserProfile(props) {
             <img src={rightArrow} alt="right-arrow" className={`w-6 h-6 ${props.mode==="light" ? "invert-0" : "invert"}`} />
           </div>
         </Link>
-        <Link>
-          <div className={`flex items-center justify-between shadow-md rounded-2xl px-2 py-1 mx-8 my-4 ${props.mode==="light" ? "text-gray-700 bg-gray-100" : "text-gray-300 bg-gray-700"}`}>
+        <Link to="/login">
+          <div className={`flex items-center justify-between shadow-md rounded-2xl px-2 py-1 my-4 ${props.mode==="light" ? "text-gray-700 bg-gray-100" : "text-gray-300 bg-gray-700"}`}>
             <div className="flex items-center">
               <img src={logoutIcon} alt="user-icon" className={`w-8 h-8 ${props.mode==="light" ? "invert-0" : "invert"}`} />
               <div className="ml-2">Logout</div>
+            </div>
+            <img src={rightArrow} alt="right-arrow" className={`w-6 h-6 ${props.mode==="light" ? "invert-0" : "invert"}`} />
+          </div>
+        </Link>
+        <Link to="/login" className="hidde">
+          <div className={`flex items-center justify-between shadow-md rounded-2xl px-2 py-1 my-4 ${props.mode==="light" ? "text-gray-700 bg-gray-100" : "text-gray-300 bg-gray-700"}`}>
+            <div className="flex items-center">
+              <img src={loginIcon} alt="user-icon" className={`w-8 h-8 ${props.mode==="light" ? "invert-0" : "invert"}`} />
+              <div className="ml-2">Login/Signup</div>
             </div>
             <img src={rightArrow} alt="right-arrow" className={`w-6 h-6 ${props.mode==="light" ? "invert-0" : "invert"}`} />
           </div>
