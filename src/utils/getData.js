c0541f0dc75e27056074f777cd0b19450be7879b
Vishal Kumar <vy10607@gmail.com>
2023-01-26 13:57:38 +0530

    Added getData and postData utility

---
+++ src/utils/getData.js
@@ -0,0 +1,11 @@
+const getData = async (url = '') => {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+    return response.json();
+}
+
+export default getData;
