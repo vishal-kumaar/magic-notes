12f8bf3cf7e7dea15b63957d5fd04afcbd61af63
Vishal Kumar <vy10607@gmail.com>
2023-01-26 16:41:03 +0530

    Connected UpdateName to DB

--- src/components/UpdateName.jsx
+++ src/components/UpdateName.jsx
@@ -1,8 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import leftArrow from "../assets/images/left_arrow.svg";
+import putData from "../utils/putData";
+import getData from "../utils/getData";
 
 export default function Login(props) {
+  const [newName, setNewName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const updateName = async () => {
+    const data = {
+      newName,
+      password
+    }
+
+    const res1 = await getData("/api/auth/profile");
+    if (res1.success === true){
+      const res2 = await putData(`/api/auth/username/update/${res1.user._id}`, data);
+      if (res2.success === true){
+        alert("Name successfully updated")
+        navigate("/profile")
+      }
+      else {
+        alert(res2.message);
+      }
+    }
+    else {
+      alert(res1.message);
+    }
+  }
+
+  const handleForm = (event) => {
+    event.preventDefault();
+    updateName();
+  }
+
   return (
     <div>
       <div className="mt-4 ml-2">
@@ -16,7 +49,7 @@ export default function Login(props) {
           />
         </Link>
       </div>
-      <form className="mx-4 sm:mx-10 md:mx-20 lg:mx-36 xl:mx-72 2xl:mx-96 my-28">
+      <form className="mx-4 sm:mx-10 md:mx-20 lg:mx-36 xl:mx-72 2xl:mx-96 my-28" onSubmit={handleForm}>
         <div
           className={`font-extrabold text-2xl mb-7 text-center ${
             props.mode === "light" ? "text-black" : "text-white"
@@ -33,6 +66,8 @@ export default function Login(props) {
                 ? "border-gray-200 bg-white text-gray-500 placeholder:text-gray-500"
                 : "border-gray-400 bg-gray-600 text-gray-100 placeholder:text-gray-100"
             }`}
+            value={newName}
+            onChange={(event) => {setNewName(event.target.value)}}
           />
         </div>
         <div className="my-6">
@@ -44,6 +79,8 @@ export default function Login(props) {
                 ? "border-gray-200 bg-white text-gray-500 placeholder:text-gray-500"
                 : "border-gray-400 bg-gray-600 text-gray-100 placeholder:text-gray-100"
             }`}
+            value={password}
+            onChange={(event) => {setPassword(event.target.value)}}
           />
         </div>
         <div className="my-6">
