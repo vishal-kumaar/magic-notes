b36bbc86021348fdbaa520c7fd58e416bdf7153a
Vishal Kumar <vy10607@gmail.com>
2023-01-26 15:29:26 +0530

    Fixed Signup

--- src/components/UpdatePassword.jsx
+++ src/components/UpdatePassword.jsx
@@ -1,8 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import leftArrow from "../assets/images/left_arrow.svg";
+import putData from "../utils/putData";
+import getData from "../utils/getData";
 
 export default function UpdatePassword(props) {
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const updatePassword = async () => {
+    const data = {
+      oldPassword,
+      newPassword,
+      confirmPassword
+    }
+
+    const res1 = await getData("/api/auth/profile");
+    if (res1.success === true){
+      const res2 = await putData(`/api/auth/password/update/${res1.user._id}`, data);
+      if (res2.success === true){
+        alert("Password successfully updated")
+        navigate("/profile")
+      }
+      else {
+        alert(res2.message);
+      }
+    }
+    else {
+      alert(res1.message);
+    }
+  }
+
+  const handleForm = (event) => {
+    event.preventDefault();
+    updatePassword();
+  }
+
   return (
     <div>
       <div className="mt-4 ml-2">
@@ -16,7 +51,7 @@ export default function UpdatePassword(props) {
           />
         </Link>
       </div>
-      <form className="mx-4 sm:mx-10 md:mx-20 lg:mx-36 xl:mx-72 2xl:mx-96 my-20">
+      <form className="mx-4 sm:mx-10 md:mx-20 lg:mx-36 xl:mx-72 2xl:mx-96 my-20" onSubmit={handleForm}>
         <div
           className={`font-extrabold text-2xl mb-7 text-center ${
             props.mode === "light" ? "text-black" : "text-white"
@@ -33,6 +68,8 @@ export default function UpdatePassword(props) {
                 ? "border-gray-200 bg-white text-gray-500 placeholder:text-gray-500"
                 : "border-gray-400 bg-gray-600 text-gray-100 placeholder:text-gray-100"
             }`}
+            value={oldPassword}
+            onChange={(event) => setOldPassword(event.target.value)}
           />
         </div>
         <div className="my-6">
@@ -44,6 +81,8 @@ export default function UpdatePassword(props) {
                 ? "border-gray-200 bg-white text-gray-500 placeholder:text-gray-500"
                 : "border-gray-400 bg-gray-600 text-gray-100 placeholder:text-gray-100"
             }`}
+            value={newPassword}
+            onChange={(event) => setNewPassword(event.target.value)}
           />
         </div>
         <div className="my-6">
@@ -55,6 +94,8 @@ export default function UpdatePassword(props) {
                 ? "border-gray-200 bg-white text-gray-500 placeholder:text-gray-500"
                 : "border-gray-400 bg-gray-600 text-gray-100 placeholder:text-gray-100"
             }`}
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
           />
         </div>
         <div className="my-6">
