3be4c5d1dd875e1132f4b71c42f148395d76f0ab
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-21 17:38:17 +0530

    Add updateUserName api

---
+++ src/apis/updateName.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const updateUserName = async (userId, data, token) => {
+  try {
+    const res = await axios.put(
+      `${REACT_APP_BASE_URL}/api/auth/username/update/${userId}`,
+      data,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+export default updateUserName;
