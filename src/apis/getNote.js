68b9699ab09074da051e36c0a87a8a7523895a64
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-21 17:05:39 +0530

    Add getNote api

---
+++ src/apis/getNote.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const getNote = async (noteId, token) => {
+  try {
+    const res = await axios.get(`${REACT_APP_BASE_URL}/api/note/getNote/${noteId}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+export default getNote;
