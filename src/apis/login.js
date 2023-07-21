f35e633b1853008b7fae7bda90d64440811ee042
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-21 17:09:04 +0530

    Fix login api

--- src/apis/login.js
+++ src/apis/login.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const login = async (data) => {
   try {
-    const res = await axios.put(`${REACT_APP_BASE_URL}/api/auth/login`, {
+    const res = await axios.post(`${REACT_APP_BASE_URL}/api/auth/login`, {
       data,
     });
     return res.data;
