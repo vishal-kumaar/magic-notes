61c617aeb5411a0d12f73bc5358f7cbd3bc111fb
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-21 17:10:01 +0530

    Add createNote api

---
+++ src/apis/createNote.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const createNote = async (title, token) => {
+  try {
+    const res = await axios.post(
+      `${REACT_APP_BASE_URL}/api/note/createNote`,
+      {
+        title,
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+export default createNote;
