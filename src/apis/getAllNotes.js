25333347b17bbd1050fbd503dd7b7983f3070a3d
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-21 16:59:25 +0530

    Add getAllNotes api

---
+++ src/apis/getAllNotes.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const getAllNotes = async (token) => {
+  try {
+    const res = await axios.get(`${REACT_APP_BASE_URL}/api/note/getNotes`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+export default getAllNotes;
