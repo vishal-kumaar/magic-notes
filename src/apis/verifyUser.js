bbe79cd36bab350d3f7bc1788dca542d1081b6dd
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-21 17:44:57 +0530

    Add verifyUser  api

---
+++ src/apis/verifyUser.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const verifyUser = async (userId, otp) => {
+  try {
+    const res = await axios.put(
+      `${REACT_APP_BASE_URL}/api/auth/user/verify/${userId}`,
+      {
+        otp
+      }
+    );
+    return res.data;
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+export default verifyUser;
