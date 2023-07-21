1fdd2ab5afa907299d38e80ca291609f8c2e6cf1
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-21 17:26:40 +0530

    Add deleteNote api

---
+++ src/apis/deleteNote.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const deleteNote = async (noteId, token) => {
+  try {
+    const res = await axios.delete(
+      `${REACT_APP_BASE_URL}/api/note/deleteNote/${noteId}`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+export default deleteNote;
